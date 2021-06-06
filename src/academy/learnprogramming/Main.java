d5687aebc8a2ae98510cd8dfa3de116500d45c8b
Romano Livi <romano_livi@outlook.com>
2021-06-06 01:11:31 -0400

    Fixed prime number method

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -21,7 +21,7 @@ public class Main {
         // For loop prime number exercise
 
         int count = 0;
-        for(int i=10; i<50; i++) {
+        for(int i=10; i<150; i++) {
             if(isPrime(i)) {
                 count++;
                 System.out.println("Number " + i + " is a prime number");
@@ -38,12 +38,12 @@ public class Main {
     }
 
     public static boolean isPrime(int n) {
-        if(n==1) {
+        if(n == 1) {
             return false;
         }
 
         for(int i=2; i <= n/2; i++) {
-            if(n % 1 == 0) {
+            if(n % i == 0) {
                 return false;
             }
         }
