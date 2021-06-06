e916a5d02f16f9f9feb51ff44af72b78ccd73bdc
Romano Livi <romano_livi@outlook.com>
2021-06-06 01:07:45 -0400

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,53 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+
+        // For loop interest rate exercise
+//
+        for(int i=2; i<9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        for(int i=8; i>1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        // For loop prime number exercise
+
+        int count = 0;
+        for(int i=10; i<50; i++) {
+            if(isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count == 3) {
+                    System.out.println("Exiting the for loop");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate/100));
+    }
+
+    public static boolean isPrime(int n) {
+        if(n==1) {
+            return false;
+        }
+
+        for(int i=2; i <= n/2; i++) {
+            if(n % 1 == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
